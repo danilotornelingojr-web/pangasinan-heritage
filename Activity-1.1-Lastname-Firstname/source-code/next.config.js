fc0d2d4399126a32906eb54bdd0905fb4f615ec2
danilotornelingojr-web <danilotornelingojr@gmail.com>
2026-09-07 12:48:53 +0800

    Fix syntax and add basePath in next.config.js

--- Activity-1.1-Lastname-Firstname/source-code/next.config.js
+++ Activity-1.1-Lastname-Firstname/source-code/next.config.js
@@ -1,9 +1,10 @@
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
+  basePath: '/pangasinan-heritage',
   images: {
-    unoptimized: true
-  }
-}
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
